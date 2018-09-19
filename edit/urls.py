65b192f49ea1995dacd241d087b6354244ddb81c
warner <warnerchen25@gmail.com>
2018-09-19 20:41:09 +0800

    linked search admin control

--- edit/urls.py
+++ edit/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-app_name='base'
+app_name='admin'
 urlpatterns = [
     path('',views.edit,name='edit'),
+    path('screate/',views.screate,name='screate'),
+    path('sread/',views.sread,name='sread'),
+    path('supdate/',views.supdate,name='supdate'),
+    path('sdelete/',views.sdelete,name='sdelete'),
 ]
--- edit/views.py
+++ edit/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def edit(request):
     return render(request,'edit/edit.html')
+
+def screate(request):
+    return render(request,'search/create.html',locals())
+
+def sread(request):
+    return render(request,'search/read.html',locals())
+
+def supdate(request):
+    return render(request,'search/update.html',locals())
+ 
+def sdelete(request):
+    return render(request,'search/delete.html',locals())
