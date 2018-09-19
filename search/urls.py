8204f925feef2f8c412669a742f7d3151d987bc3
warner <warnerchen25@gmail.com>
2018-09-19 20:05:08 +0800

    small change

--- search/urls.py
+++ search/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 from . import views
 
-app_name = "store"
+app_name = "search"
 
 urlpatterns = [
     #http://localhost:8000/member
     
-    path('',views.store,name="store"),
-    path('index/',views.index,name="index"),
-    path('create/',views.create,name="create"),
-    path('delete/<int:productid>',views.delete,name="delete"),
-    path('update/<int:productid>', views.update, name="update"),
+    path('',views.search,name="search"),
+
 ]
