c3ebbaa08cec0816fb0f5322b8b6ac1b58ecb2b5
warner <warnerchen25@gmail.com>
2018-09-19 21:11:32 +0800

    connect models to db

--- edit/views.py
+++ edit/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from search.models  import Articles, Teams
 
+articles = Articles
 # Create your views here.
 def edit(request):
     return render(request,'edit/edit.html')
@@ -8,6 +10,9 @@ def screate(request):
     return render(request,'search/create.html',locals())
 
 def sread(request):
+    data = articles.objects.all()
+    for article in data:
+        print(article.title)
     return render(request,'search/read.html',locals())
 
 def supdate(request):
--- search/models.py
+++ search/models.py
@@ -1,3 +1,29 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Articles(models.Model):
+    articleid = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    date = models.CharField(max_length=45, blank=True, null=True)
+    teamid = models.ForeignKey('Teams', models.DO_NOTHING, db_column='teamid', blank=True, null=True)
+    articletype = models.CharField(max_length=45)
+
+    class Meta:
+        db_table = 'articles'
+
+
+class Teams(models.Model):
+    teamid = models.AutoField(primary_key=True)
+    teamname = models.CharField(max_length=45)
+    teamname_eng = models.CharField(max_length=45, blank=True, null=True)
+
+    class Meta:
+        db_table = 'teams'
