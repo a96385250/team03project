ad4994c5d8c7e25cca6e58675777efdfa0abf13c
Ni <my970504@gmail.com>
2018-09-21 16:36:22 +0800

    store 0921

--- edit/views.py
+++ edit/views.py
@@ -5,7 +5,6 @@ from django.http import HttpResponse
 from store.models import Productcat,Products,Teams
 from django.core.files.storage import FileSystemStorage
 from django.utils.encoding import smart_str
-from .modelsproduct import Member
 
 
 
@@ -125,7 +124,6 @@ def aien0313del(request,id):
     playerdelete.delete()
     return redirect('../playerlist/')
 # store
-member = Member()
 
 def index(request):  
     title = "管理商品"
@@ -135,7 +133,7 @@ def index(request):
     return render(request,'store/index.html',locals())
 
 def create(request):
-    title = "產品新增"
+    title = "商品新增"
     if request.method == "POST" and request.FILES["image"]:
         categoryid = request.POST["categoryid"]
         productname = request.POST["productname"]
@@ -155,10 +153,30 @@ def create(request):
     return render(request,'store/create.html',locals())
 
 def delete(request, productid):
-    member.delete(productid)
+    products = Products.objects.get(productid=productid)
+    products.delete()
     return redirect("/edit/index/")
+
 def update(request, productid):
-    pass
+    title = "商品更新"
+    if request.method == "POST" and request.FILES["image"]:
+        categoryid = request.POST["categoryid"]
+        productname = request.POST["productname"]
+        price = request.POST["price"]
+        productdesc = request.POST["productdesc"]
+        teamid = request.POST["teamid"]
+        
+        myFile = request.FILES["image"]
+        fs = FileSystemStorage()
+        image = fs.save(myFile.name,myFile)
+
+        Products.objects.filter(productid=productid).update(productname=productname,price=price,productdesc=productdesc,teamid=teamid)
+        return redirect("/edit/index/")
+
+    products = Products.objects.get(productid=productid)
+    productcat = Productcat.objects.all()
+    teams = Teams.objects.all()
+    return render(request,'store/update.html',locals())
     
 def testencoding(request):
     u1 = "中文"
