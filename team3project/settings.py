723b0b647c7ddf240007e884f1122d8d58532f0a
aien0313 <aien0313@iii.org.tw>
2018-09-21 15:58:17 +0800

    aien0313 settings.py static

--- team3project/settings.py
+++ team3project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '!lhsw)9vkp&pv6udu7!82ui)nt$g1kw7^z^o-a7&b)g=_lj503'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,9 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR,'static')
-]
+# STATICFILES_DIRS =[
+#     os.path.join(BASE_DIR,'static')
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
