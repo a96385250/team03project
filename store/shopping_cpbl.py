e4c8e1a8fe084e1cc6fda2f466e3ecaf5c9217dc
Ni <my970504@gmail.com>
2018-10-11 14:40:37 +0800

    store 10/11

--- store/shopping_cpbl.py
+++ store/shopping_cpbl.py
@@ -5,74 +5,22 @@ import re
 import pymysql
 import csv
 
-# # 統一
-# tag="/store.aspx?type=ALL&page=1"
-# while tag:
-#     url = "https://www.uni-lions.com.tw"+tag
-#     time.sleep(2)
-#     headers_data = {
-#         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
-#     }
-#     response = requests.get(url, headers=headers_data)
-#     soup = BeautifulSoup(response.text,'lxml')
-#     items = soup.select('div.info > div.item')
-#     sites = soup.select('div.page_info > div.arrow_right')
-#     for item in items:
-#         category = "統一獅"
-#         productname = item.find('td',class_="title").get_text()
-#         test = item.find('a').get('href')
-#         match = re.search(r'(?P<other>\D+)(?P<price>\d+)(?P<other2>.+)',item.find('td',class_="price").get_text())
-#         try:
-#             other = match.group("other")
-#             other2 = match.group("other2")
-#             price = match.group("price")
-#         except AttributeError:
-#             pass
-        
-#         itag=("/{}".format(test))
-#         iurl = "https://www.uni-lions.com.tw"+itag
-#         time.sleep(2)
-#         iresponse = requests.get(iurl, headers=headers_data)
-#         isoup = BeautifulSoup(iresponse.text,'lxml')
-#         iitems = isoup.select('div.detail div.big_photo')
-#         for item in iitems:
-#             image=item.find('img').get('src')
-
-#         time.sleep(2)
-#         response = requests.get(url, headers=headers_data)
-#         soup = BeautifulSoup(response.text,'lxml')
-#         sites = soup.select('div.page_info > div.arrow_right')
-#         print(productname)
-#         print(category)
-#         print(price)
-#         print("https://www.uni-lions.com.tw/{}".format(image))
-#         print("===============")
-#     if sites:
-#         for site in sites:
-#             tag = site.find('a').get("href")
-#     else:
-#         break
-
-# # 兄弟(ajax)
-# url = "https://www.3355.com.tw/brothers/category.php?cid=73"
-# time.sleep(2)
-# headers_data = {
-#     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
-# }
-# response = requests.get(url, headers=headers_data)
-# soup = BeautifulSoup(response.text,'lxml')
-# items = soup.select('div.pdlist li.item')
-# for item in items:
-#     category = "中信兄弟"
-#     productname = item.find('h3').get_text()
-#     price = item.find('div',class_="hide").get_text()
-#     image=item.find('div',class_="img")['data-img']
-
-#     print(productname)
-#     print(category)
-#     print(price)
-#     print(image)
-#     print("===============")
+# 兄弟
+url = "https://www.3355.com.tw/brothers/category.php?cid=88"
+time.sleep(2)
+headers_data = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+}
+response = requests.get(url, headers=headers_data)
+soup = BeautifulSoup(response.text,'lxml')
+items = soup.select('div.pdlist li.item')
+for item in items:
+    teamid = "4"
+    categoryid="2"
+    productname = item.find('h3').get_text()
+    price = item.find('div',class_="hide").get_text()
+    image=item.find('div',class_="img")['data-img']
+    print(image)
 
 # # 富邦悍將(在javascript裡)
 # tag=1
