acf076946a1165823cb14f2d9c3ccfa7cfe25248
aien0313 <aien0313@iii.org.tw>
2019-05-01 17:34:30 +0800

    jjyen update

--- team3project/settings.py
+++ team3project/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'yamidb',
         'USER':'root',
-        'PASSWORD':'yen810327',
+        'PASSWORD':'XXXXX',
         'HOST':'localhost'
     }
 }
